fad2fb6098302191bd257fee7c48f2e2eed7430f
Myles Young <myles.young@mailchimp.com>
2020-01-31 08:54:30 -0500

    got the stop watch working, could be more efficient though

--- src/components/StopWatch.js
+++ src/components/StopWatch.js
@@ -1,33 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function StopWatch() {
-    let [isRunning, setIsRunning] = useState(false);
-    let [ticking, isTicking] = useState(0);
+    let [isStarted, setIsStarted] = useState(false);
+    let [count, setCount] = useState(0);
 
-    const handleStopWatch = () => {
-        setInterval(() => {
-            ticking += 1;
-        }, 1000);
-        console.log(ticking);
+    // Similar to componentDidMount and componentDidUpdate:
+    useEffect(() => {
+        const interval = setInterval(() => tick(), 1000);
+        return () => clearInterval(interval);
+    });
+
+    const tick = () => {
+        if (isStarted) setCount(count += 1);
     }
 
-    const toggleTimer = () => {
-        setIsRunning(!isRunning);
+    const reset = () => {
+        setCount(count = 0);
+    }
 
-        if (!ticking) {
-            isTicking(handleStopWatch);   
-        } 
-        else if (ticking) {
-            return;
-        }
+    const toggleStartStop = () => {
+        // handle changing button state to t & f
+        setIsStarted(!isStarted);
     }
 
     return (
         <div className="stopwatch">
             <h2>What it do babeee</h2>
-            <span className="stopwatch-time">{0}</span>
-            <button onClick={toggleTimer}>{isRunning ? "stop" : "start" }</button>
-            <button>Reset</button>
+            <span className="stopwatch-time">{count}</span>
+            <button onClick={toggleStartStop}> 
+                {isStarted ? "stop" : "start"} 
+            </button>
+            <button onClick={reset}> Reset </button>
         </div>
         
     )
